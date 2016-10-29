88ca0fb9f2c82f23cda9d5abf16dac65c06b4db8
levinqdl <levinqdl@gmail.com>
2016-10-30 01:05:22 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+let app = express();
+app.use(express.static(__dirname+'/public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+  res.render('index', {title:'Hey',message:'hello world'});
+});
+
+app.get('/share', (req, res)=>{
+  res.render('share');
+})
+
+app.get('/success', (req, res)=>{
+  res.render('success');
+})
+
+app.get('/fail', (req, res)=>{
+  res.render('fail');
+})
+app.listen(3000, ()=>{
+  console.log('app listening on port 3000!');
+});
---
+++ public/js/pick.js
@@ -0,0 +1,20 @@
+loop = function( arr, func) {
+  for ( i = 0; i < arr.length; i++)
+    func(arr[i], i);
+}
+var items = document.getElementsByClassName('item');
+var submit = document.getElementById('submit');
+
+handler = function(index) {
+  loop(items, function (elem, i) {
+    if (i === index ){
+      elem.className = 'item active';
+      submit.href='?active='+i;
+    } else {
+      elem.className = 'item';
+    }
+  })
+}
+loop(items, function(elem, index) {
+  elem.addEventListener('click', handler.bind(this, index));
+})
