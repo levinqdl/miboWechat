8ed5999b214eb0dca328159cd1ed264d62cbc734
levinqdl <levinqdl@gmail.com>
2016-11-06 18:24:35 +0800

    update

--- public/js/wxsdkInit.js
+++ public/js/wxsdkInit.js
@@ -1,4 +1,6 @@
 var HOST = 'http://movie.mizhibo.tv';
+
+var items = document.getElementsByClassName('item');
 function activeX(){
   var active = document.getElementsByClassName("active")[0];
   for ( var i = 0; i < items.length; i++ ){
@@ -7,16 +9,33 @@ function activeX(){
     }
   }
 }
-wx.ready(function(){
-  wx.onMenuShareTimeline({
-    title:'咪直播双11',
-    imgUrl:HOST+'/img/mibo.jpeg',
-    link:HOST+'/share?openid='+openid+'&active='+activeX(),
-    success: function() {
-      window.location = HOST+'/shareSuccess?openid='+openid+'&active='+activeX();
+var option = {
+  title:'咪直播双11',
+  imgUrl:HOST+'/img/mibo.jpeg',
+  link: null,
+  success: function() {
+    window.location = HOST+'/shareSuccess?openid='+openid+'&active='+activeX();
+  }
+};
+var handler = function(index) {
+  loop(items, function(elem, i) {
+    if (i === index ){
+      elem.className = 'item active';
+      option.link = HOST+'/share?openid='+openid+'&active='+activeX();
+    } else {
+      elem.className = 'item';
     }
-  });
-});
+  })
+}
+var loop = function( arr, func) {
+  for ( i = 0; i < arr.length; i++)
+    func(arr[i], i);
+}
+loop(items, function(elem, index) {
+  elem.addEventListener('click', handler.bind(this, index));
+})
+
+wx.onMenuShareTimeline(option);
 wx.error(function(res){
   console.error(JSON.stringify(res));
 });
